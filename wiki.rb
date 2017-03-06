563e0d260093d2887397cd3d585e384be5030af9
Kopcap94 <korsar94_07@mail.ru>
2017-03-06 19:05:18 +0500

    Update to v1.0.0

--- wiki.rb
+++ wiki.rb
@@ -25,7 +25,7 @@ module DiscordBot
 		def get_data_from_api
 			d =  JSON.parse(
 				HTTParty.get(
-					"http://ru.mlp.wikia.com/api.php?action=query&list=recentchanges&rclimit=50&rcprop=user|title|timestamp|ids|comment&format=json",
+					"http://ru.mlp.wikia.com/api.php?action=query&list=recentchanges&rclimit=50&rcprop=user|title|timestamp|ids|comment|sizes&format=json",
 					:verify => false
 				).body,
 				:symbolize_names => true
@@ -33,11 +33,17 @@ module DiscordBot
 
 			rcid = @config[ 'rcid' ]
 			show_uploads = @config[ 'show_uploads' ]
+			last_rcid = d[ 0 ][ :rcid ]
 
-			if d[ 0 ][ :rcid ] <= rcid then
+			if rcid == 0 then
+				@config[ 'rcid' ] = last_rcid
+				@client.save_config
 				return
 			end
-			last_rcid = d[ 0 ][ :rcid ]
+
+			if last_rcid <= rcid then
+				return
+			end
 
 			d.reverse.each do | obj |
 				if obj[ :rcid ] <= rcid then
@@ -48,24 +54,28 @@ module DiscordBot
 
 				case obj[ :type ]
 				when "log"
-					type = "Лог"
+					type = "Лог :pencil:"
 				when "edit"
-					type = "Изменение страницы"
+					type = "Изменение страницы :pencil2:"
 				when "new"
-					type = "Новая страница"
+					type = "Новая страница :paintbrush:"
 				else
-					type = "Неизвестный тип изменения"
+					type = "Неизвестный тип изменения :heavy_multiplication_x:"
 				end
 
 				url = "http://ru.mlp.wikia.com/index.php?diff=#{ obj[ :revid ] }"
-				message = "="*80 + "\n**Тип правки:** #{ type }\n**Участник:** #{ obj[ :user ] }\n**Страница:** #{ obj[ :title ] }\n"
+				message = 	"="*80 + 
+							"\n**Тип правки:** #{ type }\n" +
+							"**Участник:** #{ obj[ :user ] }\n" +
+							"**Страница:** #{ obj[ :title ] } [ http://ru.mlp.wikia.com/wiki/#{ obj[ :title ].split( "\s" ).join( "_" ) } ]\n"
+
 				if obj[ :ns ] != 6 then
-					message = message + "**Описание правки:** #{ obj[ :comment ] }\n**Изменения:** #{ url }\n" + "="*80
-				else
-					message = message + "**Изображение:** http://ru.mlp.wikia.com/index.php?title=#{ obj[ :title ].split( "\s" ).join( "_" ) }\n" + "="*80
+					message = message + 
+								"**Описание правки:** #{ obj[ :comment ] }\n" +
+								"**Изменения:** #{ obj[ :newlen ] - obj[ :oldlen ] } байт(а|ов) [ http://ru.mlp.wikia.com/index.php?diff=#{ obj[ :revid ] } ]\n"
 				end
 
-				@bot.send_message( @channels[ 'recentchanges' ], message )
+				@bot.send_message( @channels[ 'recentchanges' ], message + "="*80 )
 				sleep 1
 			end
 
