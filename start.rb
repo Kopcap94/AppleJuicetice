0af1274b80cac04eb8c3f002ca7ac417fa30d53a
Kop <korsar94_07@mail.ru>
2017-08-21 21:43:53 +0500

    v1.0.6

--- start.rb
+++ start.rb
@@ -1,9 +1,12 @@
+#!/usr/bin/env ruby
+
 require_relative './main'
 require_relative './commands'
 require_relative './plugins/vk'
 require_relative './plugins/wiki'
 require_relative './plugins/games'
 require_relative './plugins/mafia'
+require_relative './plugins/voice'
 
 _this = DiscordBot::Main.new
 _this.start
