f011bc6ab861ce9cf742c31b0775cc74e803da20
Kop <korsar94_07@mail.ru>
2017-08-24 15:26:18 +0500

    v1.0.5d1
    Random things and foice fixes.

--- plugins/voice.rb
+++ plugins/voice.rb
@@ -12,8 +12,8 @@ module DiscordBot
       @bot.command(
         :vjoin,
         min_args: 1,
-        description: "",
-        usage: ""
+        description: "Пригласить бота в голосовой канал. Требуется указать название канала.",
+        usage: "!vjoin VoiceChannel"
       ) do | e, *vс | join( e, vс.join( " " ).to_s ) end
 
       @bot.command(
@@ -42,11 +42,17 @@ module DiscordBot
       ) do | e | act( e, true, true ) end
 
       @bot.command(
-        :vvol,
+        :vvolume,
         min_args: 1,
         description: "Устанавливает уровень громкости бота. Значение указывается в % и должно находится в диапазоне от 0 до 200.",
-        usage: "!vvol 150"
-     ) do | e, v | vol( e, v.to_f ) end
+        usage: "!vvolume 150"
+      ) do | e, v | volume( e, v ) end
+
+      @bot.command(
+        :vdrop,
+        description: "Отключает бота от голосового канала. Не требует параметров.",
+        usage: "!vdrop"
+      ) do | e | vdrop( e ) end
     end
 
     def join( e, vc )
@@ -59,24 +65,29 @@ module DiscordBot
 
       vc_id = @channels[ id ][ vc ]
       @bot.voice_connect( vc_id )
-      e.message.create_reaction "\u2611"
+      e.respond "Подключился к #{ vc }!"
     end
 
     def play( e, m )
       m = URI.encode( m )
-      @bot.voice( e.server.id ).play_io( open( URI.parse( m ) ) )
+      id = e.server.id
+
+      if !in_voice( e ) then return; end
+
+      @bot.voice( e.server.id ).stop_playing #yolo
+      @bot.voice( id ).play_io( open( URI.parse( m ) ) )
       @bot.voice( e.server.id ).stop_playing
-      e.message.create_reaction "\u2611"
     end
 
     def act( e, s, t=false )
       id = e.server.id
-      if @bot.voice( id ).nil? then return; end
+
+      if !in_voice( e ) then return; end
 
       if ( s ) then
         if ( t ) then
-          @bot.voice( id ).stop_playing
-          e.message.create_reaction "\u2611"
+	        @bot.voice( e.server.id ).stop_playing
+	        e.respond "Воспроизведение остановлено."
         else
           @bot.voice( id ).pause
           e.respond "Воспроизведение поставлено на паузу."
@@ -87,15 +98,33 @@ module DiscordBot
       end
     end
 
-    def vol( e, v )
-      if v < 0 or v > 200 then
+    def volume( e, v )
+      if !in_voice( e ) then return; end
+      v = v.to_f / 100
+
+      if v < 0 or v > 2 then
         e.respond "Неправильно указано значение. Громкость указывается в процентах и входить в диапазон от 0 до 200 включительно."
         return
       end
 
-      @bot.voice( e.server.id ).volume = v / 100
-      puts v / 100
-      e.message.create_reaction "\u2611"
+      @bot.voice( e.server.id ).volume = v
+      e.respond "Громкость: #{ v*100 }%"
+    end
+
+    def vdrop( e )
+      if !in_voice( e ) then return; end
+
+      @bot.voice( e.server.id ).destroy
+      e.respond "Отключился от голосового канала."
+    end
+
+    def in_voice( e )
+      if @bot.voice( e.server.id ).nil? then
+        e.respond "На данный момент бот не находится в голосовом канале. Используйте команду !vjoin."
+        return false
+      else
+        return true
+      end
     end
   end
 end
