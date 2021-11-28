dacc7864fbdb19145db810294c1cc118257e7d5d
Kop <korsar94_07@mail.ru>
2021-11-28 11:06:29 +0500

    v1.1
    [ tcp.rb ] Rolling out Social Activity. Special IRC feed required
    [ wiki.rb ] Minor changes + cleanup

--- plugins/tcp.rb
+++ plugins/tcp.rb
@@ -6,7 +6,9 @@ module DiscordBot
       @c = client
       @bot = client.bot
       @channels = client.channels
+      @config = client.config
       @thr = client.thr
+      @names = [ 'recentchanges', 'wiki-activity', 'правки', 'активность', 'вики-активность' ]
 
       for_init
     end
@@ -21,20 +23,93 @@ module DiscordBot
       s = TCPServer.open( 5445 )
       loop {
         t = Thread.start( s.accept ) do | c |
-          msg = c.gets.chomp.to_s.force_encoding( 'UTF-8' )
- 
-          channel = @channels[ 285482504817868800 ][ 'ponyville' ]
+          data = c.gets.chomp
 
           begin
-            @bot.send_message( channel, msg )
+            d = JSON.parse( data )
+            send_info( d )
+          rescue JSON::ParserError
+            puts "Ошибка при парсе #{ data }"
           rescue => err
-            puts err.inspect
+            puts "А, ой"
           end
 
           c.close
           t.kill
         end
       }
+    end
+
+    def send_info( data )
+      emb = Discordrb::Webhooks::Embed.new
+      cat_name = "Категория"
+
+      case data[ "action" ]
+      when "deleted"
+        action = "Удалено"
+      when "created"
+        action = "Создано"
+      when "modified"
+        action = "Отредактировано"
+      when "moved"
+        action = "Перемещено"
+      when "un-delete"
+        action = "Восстановлено"
+      else
+        action = data[ "action" ]
+      end
+
+      case data[ "type" ]
+      when "discussion-thread"
+        type = "Тема в общих обсуждениях"
+        color = "#009DFF"
+      when "discussion-post"
+        type = "Ответ в теме в общих обсуждениях"
+        color = "#28ABFF"
+      when "discussion-report"
+        type = "Жалоба на нарушение!"
+        color = "#FF0000"
+      when "message-wall-thread"
+        type = "Тема на стене"
+        color = "#009E60"
+        cat_name = "У участника"
+      when "message-wall-post"
+        type = "Ответ в теме на стене"
+        color = "#33B180"
+        cat_name = "У участника"
+      when "article-comment-thread"
+        type = "Тема в обсуждениях статьи"
+        color = "#FFA701"
+      when "article-comment-reply"
+        type = "Ответ в теме в обсуждениях статьи"
+        color = "#FFFF33"
+      else
+        type = "Неопределённый тип действия: #{ data[ "type" ] }"
+        color = "#000000"
+      end
+
+      user = data[ "userName" ] == "" ? "Аноним" : data[ "userName" ]
+      title = data[ "title" ] == "" ? "-" : data[ "title" ]
+      text = data[ "snippet" ] == "" ? "-" : data[ "snippet" ]
+      category = data[ "category" ] == "" ? "-" : data[ "category" ].gsub( /\sMessage Wall/, '' )
+
+      emb.color = color
+      emb.author = Discordrb::Webhooks::EmbedAuthor.new( name: "[ #{ action } ] #{ type }", url: data[ "url" ] )
+      emb.add_field( name: "Участник", value: user, inline: true )
+      emb.add_field( name: cat_name, value: category, inline: true )
+      emb.add_field( name: "Ссылка", value: "[Ссылка](#{ data[ "url" ] })", inline: true )
+      emb.add_field( name: "Заголовок", value: title )
+      emb.add_field( name: "Текст", value: text )
+
+      @config[ "wikies" ][ data[ "wiki" ] ][ "servers" ].each do | id |
+        if @channels[ id ].nil? then next; end
+
+        s_ch = @channels[ id ].keys
+        channel = s_ch & @names
+
+        if channel.empty? then next; end
+        @bot.send_message( @channels[ id ][ channel[ 0 ] ], '', false, emb )
+      end
     end
   end
 end
